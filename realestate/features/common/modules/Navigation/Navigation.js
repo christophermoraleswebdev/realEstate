089fcb46a6037dbd6ab7fdc74ed2d35a1de6cbe5
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-08-28 12:09:18 -0500

    Navigation Completed, Footer Started

---
+++ realestate/features/common/modules/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import NavigationDesktop from "./components/NavigationDesktop/NavigationDesktop";
+import NavigationMobile from "./components/NavigationMobile/NavigationMobile";
+
+
+
+const Navigation = () => {
+  return (
+      <>
+            <NavigationDesktop />
+            <NavigationMobile />
+      </>
+  )
+}
+
+export default Navigation
---
+++ realestate/features/common/modules/Navigation/NavigationConsts.js
@@ -0,0 +1,8 @@
+import { HiPhoneArrowUpRight } from "react-icons/hi2";
+
+
+export const navigationLinks = [
+      {title: 'Properties', link:'/properties'}, 
+      {title: 'Contact', link:'/contact'}, 
+      {title: '(000) 000 0000', link:'tel:(000) 000 000', icon: <HiPhoneArrowUpRight/>}, 
+]
